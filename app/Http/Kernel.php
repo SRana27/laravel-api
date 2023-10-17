abb8d451e345dc46afc696f47a8416f4f0301124
SRana27 <sohel.ewu7@gmail.com>
2023-10-18 01:51:45 +0600

    solve

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,11 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-<<<<<<< HEAD
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-=======
-             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
->>>>>>> 66a1a1c318e5be4068f2f7211baa4296f9198c83
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -57,19 +53,6 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
-<<<<<<< HEAD
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
-        'signed' => \App\Http\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-=======
         'auth'              => \App\Http\Middleware\Authenticate::class,
         'auth.basic'        => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session'      => \Illuminate\Session\Middleware\AuthenticateSession::class,
@@ -81,6 +64,6 @@ class Kernel extends HttpKernel
         'signed'            => \App\Http\Middleware\ValidateSignature::class,
         'throttle'          => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'          => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
->>>>>>> 66a1a1c318e5be4068f2f7211baa4296f9198c83
+
     ];
 }
